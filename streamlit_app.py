064713ad3aca3a342addf853eeeeed892a90916d
Plumster60 <blplum@nd.gov>
2026-02-12 10:42:04 -0600

    Update streamlit_app.py
    , use_container_width=True has been deprecated

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,4 +43,4 @@ import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 # st.text(smoothiefroot_response.json())
 
-sf_df = st.dataframe(data=ssmoothiefroot_response.json(), use_container_width=True)
+sf_df = st.dataframe(data=ssmoothiefroot_response.json())
