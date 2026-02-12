8141df1dae0afe5a34a15a19ec73ba5b6f80d6e6
Plumster60 <blplum@nd.gov>
2026-02-12 10:42:42 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,4 +43,4 @@ import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 # st.text(smoothiefroot_response.json())
 
-sf_df = st.dataframe(data=ssmoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json())
